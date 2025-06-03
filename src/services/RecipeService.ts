f716fdc7ba51c324f227e85f64ab46c40ae1ea38
Jose David <lujan9495@gmail.com>
2025-06-03 00:17:34 -0500

    Calling to the api and validating with zod the API response

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,4 +1,14 @@
+import axios from "axios"
+import { CategoriesAPIResponseSchema } from "../schemas/recipe_schema"
+
 export async function getCategories () {
 
-  console.log("Desde ...")
+  const URL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+  const { data } = await axios.get(URL)
+  const result = CategoriesAPIResponseSchema.safeParse(data) // Validate if we type correctly the API response
+
+  if(result.success) {
+    return result.data // Return the drinks 
+  }
+
 }
