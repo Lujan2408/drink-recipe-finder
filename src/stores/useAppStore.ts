3a3d17918cbbbd5faef5f30094ef990171ebf054
Jose David <lujan9495@gmail.com>
2025-06-05 22:59:42 -0500

    creating a slice for notifications

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,10 +3,14 @@ import { devtools } from 'zustand/middleware'
 import { createRecipeSlice } from './recipeSlice'
 import { type RecipeSliceTypes } from './recipeSlice' 
 import { createFavoritesSlice, type FavoritesSliceType } from './favoritesSlice'
+import { createNotificationSlice, type NotificationSliceType } from './notificationSlice'
 
-export type RecipeDataTypes = RecipeSliceTypes & FavoritesSliceType
+export type RecipeDataTypes = RecipeSliceTypes &
+  FavoritesSliceType &
+  NotificationSliceType;
 
 export const useAppStore = create<RecipeDataTypes>()(devtools((...a) => ({
   ...createRecipeSlice(...a),
-  ...createFavoritesSlice(...a)
+  ...createFavoritesSlice(...a),
+  ...createNotificationSlice(...a)
 })))
