42187fdc5b449b43d6a6b4f55dc34ec574ba44e7
Jose David <lujan9495@gmail.com>
2025-06-05 22:41:28 -0500

    adding favorites to local Storage

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,8 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header';
 import Modal from '../components/Modal';
+import { useAppStore } from '../stores/useAppStore';
+import { useEffect } from 'react';
 
 export default function Layout() {
+
+  const loadFromLocalStorage = useAppStore((state) => state.loadFromLocalStorage)
+
+  // when our app renders, we load the data from local storage
+  useEffect(() => {
+    loadFromLocalStorage()
+  }, [loadFromLocalStorage])
+
   return (
     <>
       <Header />
--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -5,6 +5,7 @@ export type FavoritesSliceType = {
   favorites: Recipe[],
   handleClickFavorite: (recipe: Recipe) => void
   favoriteExists: (id: Recipe['idDrink']) => boolean
+  loadFromLocalStorage: () => void
 }
 
 export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get) => ({
@@ -25,10 +26,24 @@ export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get
         favorites: [ ...state.favorites, recipe ],
       }))
     }
+    // set the item to add to localStorage 
+    localStorage.setItem('favorites', JSON.stringify(get().favorites))
   },
   
   favoriteExists: (id) => {
     // some is an array method that returns true if at least one element in the array passes the test, in this case, we are checking if the recipe idDrink is equal to the id that we are passing in
     return get().favorites.some(favorite => favorite.idDrink === id)
+  },
+  
+  loadFromLocalStorage: () => {
+    // get favorites from localStorage 
+    const storedFavorites = localStorage.getItem('favorites')
+    // add to favorites state from favorites localStorage
+    if(storedFavorites) {
+      set({
+        favorites: JSON.parse(storedFavorites)
+      })
+    }
+
   }
 })
