be879721d0f07217464500c10b73f7872267d1af
Jose David <lujan9495@gmail.com>
2025-06-05 22:52:43 -0500

    checking if we have favorites and rendering conditionally each drink we have saved in favorites

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,7 +1,33 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function FavoritesPage() {
+
+  const favorites = useAppStore((state) => state.favorites)
+
+  // Check if we have favorites 
+  // useMemo is a hook that will only re-render if the value changes
+  const hasFavorites = useMemo(() => favorites.length, [favorites])
+
   return (
     <>
-      <h1 className="text-2xl">Favorites</h1>
+      <h1 className="text-6xl font-bold">Favorites</h1>
+
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {favorites.map((drink) => (
+            <DrinkCard 
+              key={drink.idDrink} 
+              drink={drink}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-2xl font-normal ">
+          No tienes favoritos aún
+        </p>
+      )}
     </>
-  )
+  );
 }
